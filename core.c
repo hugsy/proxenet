c23e54b1ba273219e6119418dfcdde34715fc539
hugsy <hugsy+github@pyc.li>
2013-09-27 19:39:14 +1000

    * improved python3 compatibility

--- core.c
+++ core.c
@@ -534,8 +534,6 @@ void proxenet_process_http_request(sock_t server_socket)
 		if( client_socket > 0 && FD_ISSET(client_socket, &rfds ) ) {
 			int n = -1;
 
-			/* proxenet_xzero(&res, sizeof(request_t)); */
-			
 			if (is_ssl)
 				n = proxenet_read_all(client_socket, &http_response, &ssl_context.client.context);
 			else
