0a20b30d7bd9a962810f7a27032e81e4a67a9974
hugsy <hugsy@pyc.li>
2015-02-04 10:29:11 +1100

    added default openssl conf

--- make.sh
+++ make.sh
@@ -5,13 +5,12 @@ ver="`git log -n 1 --pretty=format:'git-%t-%at'`"
 
 if [ "$branch" = "dev" ]; then
     echo "Building for '$branch'"
-    make clean && make VERSION_REL=$ver DEBUG=1 CC=clang NO_PYTHON=0 NO_PERL=1 NO_LUA=1 NO_RUBY=1 NO_C=1
+    make clean && make VERSION_REL=$(ver) DEBUG=1 CC=cc NO_PYTHON=0 NO_PERL=1 NO_LUA=1 NO_RUBY=1 NO_C=0
 
 elif [ "$branch" = "master" ]; then
     echo "Building for '$branch'"
-    make clean && make DEBUG=0 CC=gcc
+    make clean && make DEBUG=1 CC=cc VERSION_REL=$ver
 
 else
     echo "Unknown branch"
 fi
-
