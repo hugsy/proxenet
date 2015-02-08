616e6fbb83f03e719fd48efe9a0a3262cf33a647
hugsy <hugsy+github@pyc.li>
2015-02-09 10:40:55 +1100

    * [cmake] fixed FindRuby

--- cmake/FindRuby.cmake
+++ cmake/FindRuby.cmake
@@ -7,7 +7,6 @@
 # _RUBY_MAJOR_
 # _RUBY_MINOR_
 
-include(FindPackageHandleStandardArgs)
 
 find_program(RUBY_EXECUTABLE
   NAMES ruby1.9.3 ruby193 ruby1.9.2 ruby192 ruby1.9.1 ruby191 ruby1.9 ruby19 ruby1.8 ruby18 ruby
@@ -25,32 +24,29 @@ if(RUBY_EXECUTABLE)
     OUTPUT_VARIABLE _RUBY_MINOR_
     )
 
+  if(!RUBY_FOUND)
+    include(FindPackageHandleStandardArgs)
 
-  # Get Ruby include directories
-  execute_process(
-    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['rubyhdrdir']"
-    OUTPUT_VARIABLE RUBY_INCLUDE_HDR_DIR
-    )
-  find_path(RUBY_INCLUDE_DIR NAMES ruby.h ruby/config.h)
-  if(RUBY_INCLUDE_DIR-NOTFOUND)
-    return()
+    # Get Ruby include directories
+    find_path(RUBY_INCLUDE_DIRS NAMES ruby.h ruby/config.h)
+
+    # Get Ruby libraries directories
+    execute_process(
+      COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['RUBY_SO_NAME']"
+      OUTPUT_VARIABLE RUBY_LIB
+      )
+    find_library(RUBY_LIBRARIES NAMES ruby${_RUBY_MAJOR_}-${_RUBY_MINOR_})
+    if(RUBY_LIBRARIES-NOTFOUND)
+      return()
+    endif()
+
+    set(RUBY_LIBRARIES ${RUBY_LIBRARIES} ${RUBY_LIB})
+
+    # if here, mark ruby as found
+    set (RUBY_FOUND True)
   endif()
-  set(RUBY_INCLUDE_DIRS ${RUBY_INCLUDE_HDR_DIR} ${RUBY_INCLUDE_DIR})
 
 
-  # Get Ruby libraries directories
-  execute_process(
-    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['RUBY_SO_NAME']"
-    OUTPUT_VARIABLE RUBY_LIB
-    )
-  find_library(RUBY_LIBRARIES NAMES ruby${_RUBY_MAJOR_}-${_RUBY_MINOR_})
-  if(RUBY_LIBRARIES-NOTFOUND)
-    return()
-  endif()
-  set(RUBY_LIBRARIES ${RUBY_LIBRARIES} ${RUBY_LIB})
-
-
-  set (RUBY_FOUND True)
   mark_as_advanced(
     RUBY_FOUND
     RUBY_LIBRARIES RUBY_INCLUDE_DIRS
