f03be32b14c5f86c8e6b7a70fae80f8fa549de40
hugsy <hugsy+github@pyc.li>
2015-02-12 12:54:52 +1100

    fixed cmake package for ruby

--- cmake/FindRuby.cmake
+++ cmake/FindRuby.cmake
@@ -7,50 +7,58 @@
 # _RUBY_MAJOR_
 # _RUBY_MINOR_
 
+if(RUBY_FOUND)
+   set(RUBY_FIND_QUIETLY TRUE)
+endif()
 
 find_program(RUBY_EXECUTABLE
-  NAMES ruby1.9.3 ruby193 ruby1.9.2 ruby192 ruby1.9.1 ruby191 ruby1.9 ruby19 ruby1.8 ruby18 ruby
+  NAMES ruby2.1 ruby2.0 ruby1.9.3 ruby193 ruby1.9.2 ruby192 ruby1.9.1 ruby191 ruby1.9 ruby19 ruby1.8 ruby18 ruby
   PATHS /usr/bin /usr/local/bin /usr/pkg/bin
   )
-
 if(RUBY_EXECUTABLE)
-  # Get Ruby version
   execute_process(
     COMMAND ${RUBY_EXECUTABLE} -e "print RUBY_VERSION.split('.')[0]"
     OUTPUT_VARIABLE _RUBY_MAJOR_
     )
+
   execute_process(
     COMMAND ${RUBY_EXECUTABLE} -e "print RUBY_VERSION.split('.')[1]"
     OUTPUT_VARIABLE _RUBY_MINOR_
     )
 
-  if(!RUBY_FOUND)
-    include(FindPackageHandleStandardArgs)
-
-    # Get Ruby include directories
-    find_path(RUBY_INCLUDE_DIRS NAMES ruby.h ruby/config.h)
-
-    # Get Ruby libraries directories
-    execute_process(
-      COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['RUBY_SO_NAME']"
-      OUTPUT_VARIABLE RUBY_LIB
-      )
-    find_library(RUBY_LIBRARIES NAMES ruby${_RUBY_MAJOR_}-${_RUBY_MINOR_})
-    if(RUBY_LIBRARIES-NOTFOUND)
-      return()
-    endif()
-
-    set(RUBY_LIBRARIES ${RUBY_LIBRARIES} ${RUBY_LIB})
-
-    # if here, mark ruby as found
-    set (RUBY_FOUND True)
-  endif()
-
-
-  mark_as_advanced(
-    RUBY_FOUND
-    RUBY_LIBRARIES RUBY_INCLUDE_DIRS
-    _RUBY_MAJOR_
-    _RUBY_MINOR_
+  execute_process(
+    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['rubyhdrdir'] || RbConfig::CONFIG['archdir']"
+    OUTPUT_VARIABLE RUBY_ARCH_DIR
     )
-endif(RUBY_EXECUTABLE)
+  execute_process(
+    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['arch']"
+    OUTPUT_VARIABLE RUBY_ARCH
+    )
+  execute_process(
+    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['libdir']"
+    OUTPUT_VARIABLE RUBY_POSSIBLE_LIB_PATH
+    )
+  execute_process(
+    COMMAND ${RUBY_EXECUTABLE} -r rbconfig -e "print RbConfig::CONFIG['rubylibdir']"
+    OUTPUT_VARIABLE RUBY_RUBY_LIB_PATH
+    )
+  find_path(RUBY_INCLUDE_DIRS
+    NAMES ruby.h
+    PATHS ${RUBY_ARCH_DIR}
+    )
+  set(RUBY_INCLUDE_ARCH "${RUBY_INCLUDE_DIRS}/${RUBY_ARCH}")
+  find_library(RUBY_LIB
+    NAMES ruby-1.9.3 ruby1.9.3 ruby193 ruby-1.9.2 ruby1.9.2 ruby192 ruby-1.9.1 ruby1.9.1 ruby191 ruby1.9 ruby19 ruby1.8 ruby18 ruby
+    PATHS ${RUBY_POSSIBLE_LIB_PATH} ${RUBY_RUBY_LIB_PATH}
+    )
+  if(RUBY_LIB AND RUBY_INCLUDE_DIRS)
+    set(RUBY_FOUND TRUE)
+  endif()
+  set(RUBY_INCLUDE_DIRS "${RUBY_INCLUDE_DIRS};${RUBY_INCLUDE_ARCH}")
+  mark_as_advanced(
+    RUBY_INCLUDE_DIRS
+    RUBY_LIBRARY_DIRS
+    RUBY_LIB
+    _RUBY_MAJOR_ _RUBY_MINOR_
+    )
+endif()
