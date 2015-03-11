7e27fb884bfd91a0864fafbed8aeab6dece8b91f
hugsy <hugsy+github@pyc.li>
2015-03-12 10:41:54 +1100

    [cmake] added check for compiling only with polarssl 1.3+

--- cmake/FindPolarSSL.cmake
+++ cmake/FindPolarSSL.cmake
@@ -4,6 +4,10 @@
 # POLARSSL_FOUND
 # POLARSSL_INCLUDE_DIR
 # POLARSSL_LIBRARIES
+# POLARSSL_VERSION_MAJOR
+# POLARSSL_VERSION_MINOR
+# POLARSSL_VERSION_PATCH
+# POLARSSL_VERSION
 
 include(FindPackageHandleStandardArgs)
 
@@ -14,9 +18,35 @@ find_library(POLARSSL_LIBRARIES NAMES polarssl)
 find_package_handle_standard_args(POLARSSL REQUIRED_VARS
   POLARSSL_INCLUDE_DIR POLARSSL_LIBRARIES)
 
+if(!POLARSSL_INCLUDE_DIR AND !POLARSSL_LIBRARIES)
+  message(FATAL_ERROR "Cannot find PolarSSL library")
+  set(POLARSSL_FOUND False)
+  return()
+endif()
+
+execute_process(
+    COMMAND bash -c "echo \"#include <polarssl/version.h>\n#include <stdio.h>\nint main(){printf(POLARSSL_VERSION_STRING);return 0;}\">a.c;cc a.c -lpolarssl;./a.out;rm -f a.out a.c"
+    OUTPUT_VARIABLE POLARSSL_VERSION
+    )
+string(REPLACE "." ";" POLARSSL_VERSION_LIST ${POLARSSL_VERSION})
+
+list(GET ${POLARSSL_VERSION_LIST} 0 POLARSSL_VERSION_MAJOR)
+list(GET ${POLARSSL_VERSION_LIST} 1 POLARSSL_VERSION_MINOR)
+list(GET ${POLARSSL_VERSION_LIST} 2 POLARSSL_VERSION_PATCH)
+
+if( ${POLARSSL_VERSION} VERSION_LESS "1.3.0")
+  message(FATAL_ERROR "PolarSSL 1.3+ is required for compiling ${PROGNAME} (current is ${POLARSSL_VERSION}).")
+  set(POLARSSL_FOUND False)
+  return()
+endif()
+
 set(POLARSSL_FOUND True)
 mark_as_advanced(
   POLARSSL_FOUND
   POLARSSL_INCLUDE_DIR
   POLARSSL_LIBRARIES
+  POLARSSL_VERSION_MAJOR
+  POLARSSL_VERSION_MINOR
+  POLARSSL_VERSION_PATCH
+  POLARSSL_VERSION
   )
